3bff7c1ad8057e02e50701b97f9a1e66ce9e0caa
Mark Turingan <mnturingan@student.apc.edu.ph>
2023-01-14 21:43:31 +0800

    js-greeting

---
+++ week5/js/app.js
@@ -0,0 +1,9 @@
+let container = document.querySelector(".greet-js");
+let timeNow = new Date().getHours();
+let greeting =
+  timeNow >= 5 && timeNow < 12
+    ? "Good Morning"
+    : timeNow >= 12 && timeNow < 18
+    ? "Good Afternoon"
+    : "Good evening";
+container.innerHTML = `${greeting}`;
