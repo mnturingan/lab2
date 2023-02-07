3e410ba077bc17c130c91d589b4533546c843fb6
Mark Turingan <mnturingan@student.apc.edu.ph>
2023-02-07 21:43:28 +0800

    create db after connecting

--- week9/mysql_createdb.php
+++ week9/mysql_createdb.php
@@ -10,3 +10,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+// Create database
+$sql = "CREATE DATABASE mnturingan_myallies";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
