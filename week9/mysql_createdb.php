1d2dfe521d4e9f769e805adcfa5b47b096a03c38
Mark Turingan <mnturingan@student.apc.edu.ph>
2023-02-07 21:40:34 +0800

    connection//create db

---
+++ week9/mysql_createdb.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "webprogmi212";
+$password = "webprogmi212";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE mnturingan_myallies";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
